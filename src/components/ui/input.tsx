e8421dee07a6ec6b8536ffb156b5c18088653386
parklego <hbd9425@gmail.com>
2025-03-20 00:16:23 +0900

    test : auto focus

--- src/components/ui/input.tsx
+++ src/components/ui/input.tsx
@@ -3,11 +3,16 @@ import * as React from "react";
 import { cn } from "@/lib/utils";
 
 function Input({ className, type, ...props }: React.ComponentProps<"input">) {
+  const handleFocus = (event: React.FocusEvent<HTMLInputElement>) => {
+    event.target.blur(); // 포커스 즉시 제거
+  };
+
   return (
     <input
       type={type}
       data-slot="input"
       autoFocus={false}
+      onFocus={handleFocus}
       className={cn(
         "border-input file:text-foreground placeholder:text-muted-foreground selection:bg-primary selection:text-primary-foreground flex h-9 w-full min-w-0 rounded-md border bg-transparent px-3 py-1 text-base shadow-xs transition-[color,box-shadow] outline-none file:inline-flex file:h-7 file:border-0 file:bg-transparent file:text-sm file:font-medium disabled:pointer-events-none disabled:cursor-not-allowed disabled:opacity-50 md:text-sm",
         "focus-visible:border-ring focus-visible:ring-ring/50 focus-visible:ring-[3px]",
